04bb96070a5e5cb6ca353f22bb2e2bf9829d8b9d
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-01-14 11:38:06 +0530

    add icon library to index and add endpoints to navigate each pages in App.tsx ,add store to main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RootLayout} from "./components/RootLayout.tsx";
+import {Dashboard} from "./pages/Dashboard.tsx";
+import {Crop} from "./pages/Crop.tsx";
+import {Field} from "./pages/Field.tsx";
+import {Equipment} from "./pages/Equipment.tsx";
+import {MonitoringLog} from "./pages/MonitoringLog.tsx";
+import {Staff} from "./pages/Staff.tsx";
+import {User} from "./pages/User.tsx";
+import {Vehicle} from "./pages/Vehicle.tsx";
+import {SignIn} from "./pages/SignIn.tsx";
+
+function App() {
+   const routes=createBrowserRouter([
+       {
+           path: '',
+           element : <RootLayout/>,
+           children : [
+               { path : '', element : <Dashboard/>},
+               { path : '/crop', element : <Crop/>},
+               { path : '/field', element : <Field/>},
+               { path : '/equipment', element : <Equipment/>},
+               {path:'/monitoringLog', element:<MonitoringLog/>},
+               {path:'/staff',element:<Staff/>},
+               {path:'/user',element:<User/>},
+               {path:'/vehicle',element:<Vehicle/>},
+               {path:'/login',element:<SignIn/>}
+           ]
+       },
+
+   ])
+
+  return (
+    <>
+        <RouterProvider router={routes}>
+
+        </RouterProvider>
+    </>
+  )
+}
+
+export default App
