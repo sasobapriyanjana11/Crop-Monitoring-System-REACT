1f3bdac7d4d4ad1ad86f489a209edb212a196efd
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-01-14 11:33:55 +0530

    create store.ts and add reducers

---
+++ src/store/store.ts
@@ -0,0 +1,26 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import cropSlice from "../reducers/CropSlice";
+import equipmentSlice from "../reducers/EquipmentSlice";
+import staffSlice from "../reducers/StaffSlice";
+import vehicleSlice from "../reducers/VehicleSlice.tsx";
+import fieldSlice from "../reducers/FieldSlice";
+import monitoringLogSlice from "../reducers/MonitoringLogSlice";
+import authSlice from "../reducers/AuthSlice.tsx";
+
+const rootReducer = combineReducers({
+    crops: cropSlice,
+    equipments:equipmentSlice,
+    staff: staffSlice,
+    vehicles: vehicleSlice,
+    fields:fieldSlice,
+    mLogs:monitoringLogSlice,
+    auth:authSlice,
+    // Add other reducers (e.g., equipments, staff, vehicles) if needed.
+});
+
+export const store = configureStore({
+    reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
